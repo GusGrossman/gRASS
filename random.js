9b8f4803dd9d63ba33665c3b410705b73db0bb01
GusGrossman <56972999+GusGrossman@users.noreply.github.com>
2022-02-16 23:01:27 -0500

    Add files via upload

---
+++ random.js
@@ -0,0 +1,20 @@
+  function show_image(src, width, height, alt) {
+      var img = document.createElement("img");
+      img.src = src;
+      img.width = width;
+      img.height = height;
+      img.alt = alt;
+      img.style.position = 'absolute';
+      img.style.top = document.body.clientHeight * Math.random() + 'px';
+      img.style.left = document.body.clientWidth * Math.random() + 'px';
+
+      document.body.appendChild(img);
+    }
+    document.getElementById('touch').addEventListener('click', () =>
+      show_image("data/blade.png", 17, 170,"touch")
+    );
+    var clicks = 0;
+    function onClick() {
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+    };
