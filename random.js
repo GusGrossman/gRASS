0e5717623035e88024c34c1f0cb8a90a15b24b5d
GusGrossman <56972999+GusGrossman@users.noreply.github.com>
2022-02-16 23:26:00 -0500

    Update random.js

--- random.js
+++ random.js
@@ -18,3 +18,11 @@
         clicks += 1;
         document.getElementById("clicks").innerHTML = clicks;
     };
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
